085360622277531871cd1fc737b5539af9720707
Robert Rosborg <rosborg.robert@gmail.com>
2023-11-11 14:51:38 +0100

    Setup repositories in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:workout/app.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:workout_model/workout_model.dart';
 
-void main() => runApp(const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Needs to run before getApplicationDocumentsDirectory()
+  Directory documentsDirectory = await getApplicationDocumentsDirectory(); // Platform agnostic document path
+  await ExerciseRepository.instance.initialize(filePath: documentsDirectory.path);
+  await WorkoutRepository.instance.initialize(filePath: documentsDirectory.path);
+
+  runApp(const App());
+}
