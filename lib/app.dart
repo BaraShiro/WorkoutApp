a30ec033c582a261e45f4ebdb5c1490fc4862395
Robert Rosborg <rosborg.robert@gmail.com>
2023-11-15 07:54:30 +0100

    Update app to Material 3

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,7 @@
-import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:workout/dashboard/dashboard.dart';
 import 'package:workout/dashboard/view/dashboard_page.dart';
-import 'package:workout/loading/loading.dart';
-import 'package:workout_model/workout_model.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -15,7 +11,9 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Workout',
       theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
+        useMaterial3: true,
+          colorSchemeSeed: Colors.blue,
+        // primarySwatch: Colors.lightBlue,
       ),
       home: DashboardPage(),
     );
